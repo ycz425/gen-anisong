8e19065991e3536e7ddd8970c9833d7bd4b19d65
John Zhang <23zhanyuc@gmail.com>
2025-02-03 16:48:22 -0500

    Update CustomMIDIModel forward method

--- src/midi_model_wrapper.py
+++ src/midi_model_wrapper.py
@@ -6,16 +6,19 @@ class CustomMIDIModel(MIDIModel):
     def __init__(self, config: MIDIModelConfig):
         super().__init__(config)
 
-    def forward(self, x, labels, cache=None):
-        hidden = super().forward(x, cache)
-        hidden = hidden.reshape(-1, hidden.shape[-1])
-        labels = labels.reshape(-1, labels.shape[-1])  # (batch_size*midi_sequence_length, token_sequence_length)
-        x = labels[:, :-1]
-        logits = super().forward_token(hidden, x)
-        loss = F.cross_entropy(
-            logits.view(-1, self.tokenizer.vocab_size),
-            labels.view(-1),
-            reduction="mean",
-            ignore_index=self.tokenizer.pad_id
-        )
-        return {'loss': loss, 'logits': logits}
+    def forward(self, x, labels=None, cache=None):
+        if labels is None:
+            return super().forward(x, cache=cache)
+        else:
+            hidden = super().forward(x, cache=cache)
+            hidden = hidden.reshape(-1, hidden.shape[-1])
+            labels = labels.reshape(-1, labels.shape[-1])  # (batch_size*midi_sequence_length, token_sequence_length)
+            x = labels[:, :-1]
+            logits = super().forward_token(hidden, x)
+            loss = F.cross_entropy(
+                logits.view(-1, self.tokenizer.vocab_size),
+                labels.view(-1),
+                reduction="mean",
+                ignore_index=self.tokenizer.pad_id
+            )
+            return {'loss': loss, 'logits': logits}
