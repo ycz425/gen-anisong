ba2eccfd343b4b98bd690674defea96d70ce28d9
John Zhang <23zhanyuc@gmail.com>
2025-02-13 16:58:06 -0500

    Add dropout after self-attention and MLP layers

--- src/custom_midi_model.py
+++ src/custom_midi_model.py
@@ -1,11 +1,41 @@
 from third_party.midi_model import MIDIModel, MIDIModelConfig
 import torch.nn.functional as F
-import torch
+import torch.nn as nn
+from transformers.models.llama.modeling_llama import LlamaAttention, LlamaMLP, LlamaConfig
+
+
+class LlamaAttentionWithDropout(LlamaAttention):
+    def __init__(self, config: LlamaConfig, layer_idx: int, p: float):
+        super().__init__(config, layer_idx)
+        self.dropout = nn.Dropout(p)
+
+    def forward(self, *args, **kwargs):
+        attn_output, attn_weights = super().forward(*args, **kwargs)
+        return self.dropout(attn_output), attn_weights
+    
+
+class LlamaMLPWithDropout(LlamaMLP):
+    def __init__(self, config, p: float):
+        super().__init__(config)
+        self.dropout = nn.Dropout(p)
+
+    def forward(self, x):
+        down_proj = self.down_proj(self.dropout(self.act_fn(self.gate_proj(x)) * self.up_proj(x)))
+        return down_proj
 
 
 class CustomMIDIModel(MIDIModel):
     def __init__(self, config: MIDIModelConfig):
         super().__init__(config)
+
+        for layer in self.net.layers:
+            layer.self_attn = LlamaAttentionWithDropout(layer.self_attn.config, layer.self_attn.layer_idx, p=0.2)
+            layer.mlp = LlamaMLPWithDropout(layer.mlp.config, p=0.2)
+
+        for layer in self.net_token.layers:
+            layer.self_attn = LlamaAttentionWithDropout(layer.self_attn.config, layer.self_attn.layer_idx, p=0.2)
+            layer.mlp = LlamaMLPWithDropout(layer.mlp.config, p=0.2)
+
 
     def forward(self, x, labels=None, cache=None):
         # print(f"Allocated memory: {torch.mps.current_allocated_memory() / 1e9:.2f} GB")
