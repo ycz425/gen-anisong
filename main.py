40c85231ebe426e9f9f93f4533421abd5ba5858d
John Zhang <23zhanyuc@gmail.com>
2025-02-13 16:59:19 -0500

    Refactor train_midi_model

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+from src.custom_midi_model import CustomMIDIModel
 from src.model_run import train_midi_model, generate
+from src.preprocess import create_datasets
 from transformers import TrainingArguments
 
+
+model = CustomMIDIModel.from_pretrained("skytnt/midi-model-tv2o-medium")
+train_dataset, eval_dataset = create_datasets('data/fonzi', model.tokenizer, 0.15, 3000)
 args = TrainingArguments(
-    output_dir='checkpoints/trial_4',
+    output_dir='checkpoints/trial_5',
     per_device_train_batch_size=1,
     per_device_eval_batch_size=1,
-    gradient_accumulation_steps=16,
+    gradient_accumulation_steps=28,
     lr_scheduler_type='constant',
     learning_rate=0.00002,
     weight_decay=0.0002,
@@ -17,8 +22,22 @@ args = TrainingArguments(
     bf16=True,
     save_strategy='best',
     metric_for_best_model='eval_loss',
-    save_total_limit=1,
+    save_total_limit=1
 )
 
+print(model)
 if __name__ == '__main__':
-    train_midi_model(net_layers=4, net_token_layers=3, args=args)
+    train_midi_model(
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        model=model,
+        net_layers=4,
+        net_token_layers=3,
+        args=args
+    )
+    pass
+    # generate(
+    #     checkpoint_path="skytnt/midi-model-tv2o-medium",
+    #     save_path='results/test0.mid',
+    #     max_len=2048
+    # )
--- src/model_run.py
+++ src/model_run.py
@@ -1,14 +1,13 @@
 from typing import Any
 from transformers import TrainingArguments
+from datasets import Dataset
 from src.custom_trainer import CustomTrainer
 from src.custom_midi_model import CustomMIDIModel
 from src.preprocess import create_datasets
 from src.midi_data_collator import MIDIDataCollator
 
 
-def train_midi_model(net_layers: int, net_token_layers: int, args: TrainingArguments) -> None:
-    model = CustomMIDIModel.from_pretrained("skytnt/midi-model-tv2o-medium")
-
+def train_midi_model(model: CustomMIDIModel, train_dataset: Dataset, eval_dataset: Dataset, net_layers: int, net_token_layers: int, args: TrainingArguments) -> None:
     for param in model.parameters():
         param.requires_grad = False
     for param in model.lm_head.parameters():
@@ -17,8 +16,6 @@ def train_midi_model(net_layers: int, net_token_layers: int, args: TrainingArgum
         param.requires_grad = True
     for param in model.net_token.layers[-net_token_layers:].parameters():
         param.requires_grad = True
-
-    train_dataset, eval_dataset = create_datasets('data/fonzi', model.tokenizer, 0.1, 3000)
 
     train_collator = MIDIDataCollator(model.tokenizer, train=True)
     eval_collator = MIDIDataCollator(model.tokenizer, train=False)
@@ -34,13 +31,13 @@ def train_midi_model(net_layers: int, net_token_layers: int, args: TrainingArgum
     trainer.train()
 
 
-def generate(checkpoint_path: str, save_path: str, prompt: Any = None, max_length: int = 512, temp: float = 1, top_p: float = 0.98, top_k: int = 20) -> None:
+def generate(checkpoint_path: str, save_path: str, prompt: Any = None, max_len: int = 512, temp: float = 1, top_p: float = 0.98, top_k: int = 20) -> None:
     model = CustomMIDIModel.from_pretrained(checkpoint_path)
     sequence = model.generate(
             prompt=prompt,
             batch_size=1,
-            max_length=max_length,
-            temperature=temp,
+            max_len=max_len,
+            temp=temp,
             top_p=top_p,
             top_k=top_k
     )[0]
