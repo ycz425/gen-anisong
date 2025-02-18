208630e3a025054cb2fb84ca06205949191803b9
John Zhang <23zhanyuc@gmail.com>
2025-02-18 01:14:28 -0500

    Add new data processing steps

--- src/preprocess.py
+++ src/preprocess.py
@@ -10,6 +10,9 @@ def process_midi(midi_file: str, tokenizer: MIDITokenizerV2):
 
     score = MIDI.midi2score(data)
     score = merge_tracks(score)
+    
+    score = remove_control_change_events(score)
+    score = remove_empty_first_measure(score)
     tokenized_midi = tokenizer.tokenize(score)
 
     return tokenized_midi
@@ -20,6 +23,48 @@ def merge_tracks(score):
     for track in score[1:]:
         output[1].extend(track)
     return output
+
+
+def remove_empty_first_measure(score):
+    """score must have only one track.
+    """
+    if len(score) > 2:
+        raise ValueError('Multiple tracks found in score.')
+    
+    ticks, track = score
+    time_signature = None
+
+    for event in track:
+        if event[0] == 'time_signature' and event[1] == 0:
+            time_signature = (event[2], event[3])
+            break
+
+    if time_signature is None:
+        raise ValueError('Initial time signature is missing.')
+
+    n, d = time_signature[0], 2 ** time_signature[1]
+    threshold = (4 / d * n - 0.125) * ticks
+
+    while True:
+        for event in track:
+            if event[0] == 'note' and event[1] < threshold:
+                return score
+            elif event[0] == 'note':
+                break
+
+        for event in track:
+            event[1] = max(event[1] - 4 / d * n * ticks, 0)
+
+
+def remove_control_change_events(score):
+    """score must have only one track.
+    """
+    new_score = [score[0], []]
+    for event in score[1]:
+        if event[0] != 'control_change':
+            new_score[1].append(event)
+
+    return new_score
 
 
 def create_datasets(dataset_dir: str, tokenizer: MIDITokenizerV2, val_split: float, max_sequence_length: int = 3000) -> tuple[Dataset, Dataset]:
